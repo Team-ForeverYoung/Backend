784931523cc448646fc364e092f5898246ce5c16
woosungking <abuga991008@gmail.com>
2025-04-06 18:43:45 +0900

    feat: User Event Entity

---
+++ src/main/java/com/java/backend/domain/promotion/entity/UserEvent.java
@@ -0,0 +1,56 @@
+package com.java.backend.domain.promotion.entity;
+
+import com.java.backend.domain.user.entity.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "userevents")
+public class UserEvent {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "coupon_id")
+	private Coupon coupon;
+
+	@ManyToOne
+	@JoinColumn(name = "event_id")
+	private Event event;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	public Long getId() {
+		return id;
+	}
+
+	public Coupon getCoupon() {
+		return coupon;
+	}
+
+	public Event getEvent() {
+		return event;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public UserEvent() {
+	}
+
+	public UserEvent(Coupon coupon, Event event, User user) {
+		this.coupon = coupon;
+		this.event = event;
+		this.user = user;
+	}
+}
