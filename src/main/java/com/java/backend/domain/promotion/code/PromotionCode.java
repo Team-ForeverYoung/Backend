e4fae8190edebfabe039b32af17d2a8855492260
woosungking <abuga991008@gmail.com>
2025-04-06 22:27:51 +0900

    feat: Success Code 추가

--- src/main/java/com/java/backend/domain/promotion/code/PromotionCode.java
+++ src/main/java/com/java/backend/domain/promotion/code/PromotionCode.java
@@ -6,7 +6,9 @@ import com.java.backend.global.code.ResponseApiCode;
 
 public enum PromotionCode implements ResponseApiCode {
 
-	COUPON_CREATED_SUCCESS(HttpStatus.CREATED,"c001","쿠폰이 성공적으로 등록 되었습니다"),
+	COUPON_CREATED_SUCCESS(HttpStatus.CREATED,"c001","쿠폰이 성공적으로 등록 되었습니다."),
+	EVENT_CREATED_SUCCESS(HttpStatus.CREATED,"e001","이벤트가 성공적으로 등록 되었습니다."),
+	USER_EVENT_CREATED_SUCCESS(HttpStatus.CREATED,"ue001","이벤트에 성공적으로 참여 했습니다"),
 	;
 
 	private final HttpStatus httpStatus;
