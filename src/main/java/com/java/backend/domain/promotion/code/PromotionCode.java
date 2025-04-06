77d1d78d6330e6a5cb9cfc8fc0fe028246440cef
woosungking <abuga991008@gmail.com>
2025-04-06 18:45:16 +0900

    feat: 프로모션 도메인 커스텀 응답코드

---
+++ src/main/java/com/java/backend/domain/promotion/code/PromotionCode.java
@@ -0,0 +1,34 @@
+package com.java.backend.domain.promotion.code;
+
+import org.springframework.http.HttpStatus;
+
+import com.java.backend.global.code.ResponseApiCode;
+
+public enum PromotionCode implements ResponseApiCode {
+
+	COUPON_CREATED_SUCCESS(HttpStatus.CREATED,"c001","쿠폰이 성공적으로 등록 되었습니다"),
+	;
+
+	private final HttpStatus httpStatus;
+	private final String code;
+	private final String message;
+	PromotionCode(HttpStatus httpStatus, String code, String message) {
+		this.httpStatus = httpStatus;
+		this.code = code;
+		this.message = message;
+	}
+	@Override
+	public HttpStatus getStatus() {
+		return httpStatus;
+	}
+
+	@Override
+	public String getCode() {
+		return code;
+	}
+
+	@Override
+	public String getMessage() {
+		return message;
+	}
+}
