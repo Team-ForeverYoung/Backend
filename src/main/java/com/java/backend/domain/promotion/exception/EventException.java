a6d41045f8891e5d4a49a1d9c515dc0530780920
woosungking <abuga991008@gmail.com>
2025-04-07 18:33:52 +0900

    feat: eventNotFoundException 추가

--- src/main/java/com/java/backend/domain/promotion/exception/EventException.java
+++ src/main/java/com/java/backend/domain/promotion/exception/EventException.java
@@ -1,5 +1,6 @@
 package com.java.backend.domain.promotion.exception;
 
+import com.java.backend.domain.promotion.code.PromotionErrorCode;
 import com.java.backend.global.exception.BusinessException;
 import com.java.backend.global.exception.ExceptionMetaData;
 
@@ -9,8 +10,14 @@ public abstract class EventException extends BusinessException {
 		super(exceptionMetaData);
 	}
 
-	public static class eventSoldedOutException extends EventException{
-		public eventSoldedOutException(ExceptionMetaData exceptionMetaData) {
+	public static class eventSoledOutException extends EventException{
+		public eventSoledOutException(ExceptionMetaData exceptionMetaData) {
+			super(exceptionMetaData);
+		}
+	}
+
+	public static class eventNotFoundException extends EventException{
+		public eventNotFoundException(ExceptionMetaData exceptionMetaData) {
 			super(exceptionMetaData);
 		}
 	}
