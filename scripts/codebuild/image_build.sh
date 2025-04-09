bae67aab075dc83b58d014f7ba18a8f4c3beb28c
woosungking <abuga991008@gmail.com>
2025-04-09 16:22:26 +0900

    feat: 도커 이미지 빌드 스크립트 작성

---
+++ scripts/codebuild/image_build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ "$CODEBUILD_SOURCE_VERSION" == refs/heads/* ]]; then
+  BRANCH=${CODEBUILD_SOURCE_VERSION#refs/heads/}
+elif [[ "$CODEBUILD_WEBHOOK_TRIGGER" == branch/* ]]; then
+  BRANCH=${CODEBUILD_WEBHOOK_TRIGGER#branch/}
+else
+  echo "브렌치 이름을 추출 할 수 없습니다 codebuild에 담긴 환경변수를 확인하세요"
+  echo "환경변수 CODEBUILD_SOURCE_VERSION:$CODEBUILD_SOURCE_VERSION"
+  echo "환경변수 CODEBUILD_WEBHOOK_TRIGGER:$CODEBUILD_WEBHOOK_TRIGGER"
+  exit 1;
+fi
+
+case $BRANCH in
+  develop)
+    docker build -t "$DOCKER_USER_NAME/foreverBE:DEV-$CODEBUILD_WEBHOOK_PREV_COMMIT" ../../.
+    ;;
+  main)
+    docker build -t "$DOCKER_USER_NAME/foreverBE:PROD-$CODEBUILD_WEBHOOK_PREV_COMMIT" ../../.
+    ;;
+  *)
+    echo "지원되지 않는 브랜치: $BRANCH"
+    exit 1
+    ;;
+esac
