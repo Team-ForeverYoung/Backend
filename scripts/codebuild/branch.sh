df5247889413c344bf9fbb0e3323aca3ddba2ea2
woosungking <abuga991008@gmail.com>
2025-04-09 16:23:03 +0900

    feat:

--- scripts/codebuild/branch.sh
+++ scripts/codebuild/branch.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
+#$CODEBUILD_SOURCE_VERSION" == refs/heads/*
+#$CODEBUILD_WEBHOOK_TRIGGER" == branch/*
 
-# 브랜치 이름 추출 로직
 if [[ "$CODEBUILD_SOURCE_VERSION" == refs/heads/* ]]; then
   BRANCH=${CODEBUILD_SOURCE_VERSION#refs/heads/}
 elif [[ "$CODEBUILD_WEBHOOK_TRIGGER" == branch/* ]]; then
   BRANCH=${CODEBUILD_WEBHOOK_TRIGGER#branch/}
 else
-  echo "❌ 오류: 브랜치 이름을 추출할 수 없습니다."
-  echo "CODEBUILD_SOURCE_VERSION: $CODEBUILD_SOURCE_VERSION"
-  echo "CODEBUILD_WEBHOOK_TRIGGER: $CODEBUILD_WEBHOOK_TRIGGER"
-  exit 1
+  echo "브렌치 이름을 추출 할 수 없습니다 codebuild에 담긴 환경변수를 확인하세요"
+  echo "환경변수 CODEBUILD_SOURCE_VERSION:$CODEBUILD_SOURCE_VERSION"
+  echo "환경변수 CODEBUILD_WEBHOOK_TRIGGER:$CODEBUILD_WEBHOOK_TRIGGER"
+  exit 1;
 fi
 
-# 브랜치 조건별 로직 처리
-case "$BRANCH" in
+case $BRANCH in
   main)
-    echo "✅ Main 브랜치 빌드 시작"
+    echo "메인 브렌치 빌드 시작"
     ;;
   develop)
-    echo "✅ Develop 브랜치 빌드 시작"
+    echo "디벨롭 브렌치 빌드 시작"
     ;;
   *)
-    echo "❌ 오류: 지원되지 않는 브랜치 ($BRANCH)"
+    echo "메인 또는 디벨롭 브렌치가 아님 종료할게요"
     exit 1
-    ;;
 esac
+
+export BRANCH
