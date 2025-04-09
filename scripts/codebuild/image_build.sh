88ad85eba827527895dcfd08129b87bee53b488b
woosungking <abuga991008@gmail.com>
2025-04-09 16:28:35 +0900

    feat: 이미지 푸시 및 buildx

--- scripts/codebuild/image_build.sh
+++ scripts/codebuild/image_build.sh
@@ -5,7 +5,7 @@ if [[ "$CODEBUILD_SOURCE_VERSION" == refs/heads/* ]]; then
 elif [[ "$CODEBUILD_WEBHOOK_TRIGGER" == branch/* ]]; then
   BRANCH=${CODEBUILD_WEBHOOK_TRIGGER#branch/}
 else
-  echo "브렌치 이름을 추출 할 수 없습니다 codebuild에 담긴 환경변수를 확인하세요"
+  echo "브렌치 이름을 추출 할 수 없습니다. codebuild에 담긴 환경변수를 확인하세요"
   echo "환경변수 CODEBUILD_SOURCE_VERSION:$CODEBUILD_SOURCE_VERSION"
   echo "환경변수 CODEBUILD_WEBHOOK_TRIGGER:$CODEBUILD_WEBHOOK_TRIGGER"
   exit 1;
@@ -13,10 +13,14 @@ fi
 
 case $BRANCH in
   develop)
-    docker build -t "$DOCKER_USER_NAME/foreverBE:DEV-$CODEBUILD_WEBHOOK_PREV_COMMIT" ../../.
+    TAG="DEV-$CODEBUILD_WEBHOOK_PREV_COMMIT"
+    docker buildx create --use
+    docker buildx build --push -t "$DOCKER_USER_NAME/foreverBE:$TAG" ../../.
     ;;
   main)
-    docker build -t "$DOCKER_USER_NAME/foreverBE:PROD-$CODEBUILD_WEBHOOK_PREV_COMMIT" ../../.
+    TAG="PROD-$CODEBUILD_WEBHOOK_PREV_COMMIT"
+    docker buildx create --use
+    docker buildx build --push -t "$DOCKER_USER_NAME/foreverBE:$TAG" ../../.
     ;;
   *)
     echo "지원되지 않는 브랜치: $BRANCH"
