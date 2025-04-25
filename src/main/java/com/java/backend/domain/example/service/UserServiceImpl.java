d632dc792d0eacd8ac9beb6a9489cd2d6b57f14c
woosungking <abuga991008@gmail.com>
2025-04-25 22:33:05 +0900

    chore:

--- src/main/java/com/java/backend/domain/example/service/UserServiceImpl.java
+++ src/main/java/com/java/backend/domain/example/service/UserServiceImpl.java
@@ -1,52 +1,52 @@
-package com.java.backend.domain.example.service;
-
-import org.springframework.stereotype.Service;
-
-import com.java.backend.domain.example.dto.UserRegisterRequestDto;
-import com.java.backend.domain.user.entity.User;
-import com.java.backend.domain.example.repository.OliveUserRepository;
-
-@Service
-public class UserServiceImpl implements UserService {
-    private final OliveUserRepository oliveUserRepository;
-
-    public UserServiceImpl(OliveUserRepository oliveUserRepository) {
-        this.oliveUserRepository = oliveUserRepository;
-    }
-
-    @Override
-    public void registerOliveUser(UserRegisterRequestDto requestDto) {
-        User newUser = createUserFromDto(requestDto);
-        oliveUserRepository.save(newUser);
-    }
-
-    @Override
-    public String findPasswordByUserId(String userId) {
-        return oliveUserRepository.findPassWordByUserId(userId);
-    }
-
-    @Override
-    public User login(String userId, String passWord) {
-        User user = oliveUserRepository.findByUserId(userId)
-                .orElseThrow(() -> new IllegalArgumentException("아이디가 존재하지 않습니다."));
-
-        if (!user.getPassWord().equals(passWord)) {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-        }
-
-        return user;
-    }
-
-
-
-    private User createUserFromDto(UserRegisterRequestDto dto) {
-        return User.builder()
-                .userId(dto.getUserId()) // 사용자 ID 설정
-                .name(dto.getName())
-                .email(dto.getEmail())
-                .passWord(dto.getPassWord())
-                .point(dto.getPoint())
-                .country(dto.getCountry())
-                .build();
-    }
-}
+// package com.java.backend.domain.example.service;
+//
+// import org.springframework.stereotype.Service;
+//
+// import com.java.backend.domain.example.dto.UserRegisterRequestDto;
+// import com.java.backend.domain.user.entity.User;
+// import com.java.backend.domain.example.repository.OliveUserRepository;
+//
+// @Service
+// public class UserServiceImpl implements UserService {
+//     private final OliveUserRepository oliveUserRepository;
+//
+//     public UserServiceImpl(OliveUserRepository oliveUserRepository) {
+//         this.oliveUserRepository = oliveUserRepository;
+//     }
+//
+//     @Override
+//     public void registerOliveUser(UserRegisterRequestDto requestDto) {
+//         User newUser = createUserFromDto(requestDto);
+//         oliveUserRepository.save(newUser);
+//     }
+//
+//     @Override
+//     public String findPasswordByUserId(String userId) {
+//         return oliveUserRepository.findPassWordByUserId(userId);
+//     }
+//
+//     @Override
+//     public User login(String userId, String passWord) {
+//         User user = oliveUserRepository.findByUserId(userId)
+//                 .orElseThrow(() -> new IllegalArgumentException("아이디가 존재하지 않습니다."));
+//
+//         if (!user.getPassWord().equals(passWord)) {
+//             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+//         }
+//
+//         return user;
+//     }
+//
+//
+//
+//     private User createUserFromDto(UserRegisterRequestDto dto) {
+//         return User.builder()
+//                 .userId(dto.getUserId()) // 사용자 ID 설정
+//                 .name(dto.getName())
+//                 .email(dto.getEmail())
+//                 .passWord(dto.getPassWord())
+//                 .point(dto.getPoint())
+//                 .country(dto.getCountry())
+//                 .build();
+//     }
+// }
