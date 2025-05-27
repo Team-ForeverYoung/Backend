13f4b64adeec88e8231b0f5af640ec9096204590
woosungking <abuga991008@gmail.com>
2025-05-27 12:25:19 +0900

    feat: kafka consumer

---
+++ src/main/java/com/java/backend/domain/promotion/message/PromotionEventConsumerKafka.java
@@ -0,0 +1,30 @@
+package com.java.backend.domain.promotion.message;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import com.java.backend.domain.promotion.dto.EventJoinMessage;
+import com.java.backend.domain.promotion.dto.EventJoinRequestDto;
+import com.java.backend.domain.promotion.service.EventService;
+
+@Component
+public class PromotionEventConsumerKafka implements PromotionEventConsumer{
+	private final EventService eventService;
+	private static final String SUMMER_EVENT = "SummerEvent";
+
+	public PromotionEventConsumerKafka(EventService eventService) {
+		this.eventService = eventService;
+	}
+
+	@Override
+	@KafkaListener(topics = "promotion_event")
+	public void promotionEventJoinConsumer(EventJoinMessage message) {
+		switch (message.getPromotionKey()){
+			case SUMMER_EVENT:
+				eventService.joinEvent(message.getEventJoinRequestDto());
+				break;
+			default:
+				break;
+		}
+	}
+}
