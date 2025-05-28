5563fb581c5aa3e0ff09554621b034c1aa9c149c
woosungking <abuga991008@gmail.com>
2025-05-28 20:17:55 +0900

    refactor:

--- src/main/java/com/java/backend/domain/promotion/dto/EventResultMessage.java
+++ src/main/java/com/java/backend/domain/promotion/dto/EventResultMessage.java
@@ -1,11 +1,25 @@
 package com.java.backend.domain.promotion.dto;
 
+import com.java.backend.domain.promotion.entity.UserEvent;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 public class EventResultMessage {
-	private boolean success;
-	private Long userId;
-	private Long eventId;
+	private final boolean success;
+	private final Long userId;
+	private final Long eventId;
 
+	public EventResultMessage(boolean success, Long userId, Long eventId) {
+		this.success = success;
+		this.userId = userId;
+		this.eventId = eventId;
+	}
+
+	public EventResultMessage(UserEvent userEvent, boolean success){
+		this.success = success;
+		this.userId = userEvent.getUser().getId();
+		this.eventId = userEvent.getEvent().getId();
+	}
 }
