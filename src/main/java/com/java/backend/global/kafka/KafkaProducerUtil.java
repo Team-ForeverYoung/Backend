79072a6e534b38870895301fbe2a425fef96e093
woosungking <abuga991008@gmail.com>
2025-05-28 20:19:19 +0900

    feat: eventResultKafkaTemplate 추가

--- src/main/java/com/java/backend/global/kafka/KafkaProducerUtil.java
+++ src/main/java/com/java/backend/global/kafka/KafkaProducerUtil.java
@@ -6,16 +6,21 @@ import org.springframework.stereotype.Component;
 import org.springframework.kafka.core.KafkaTemplate;
 
 import com.java.backend.domain.promotion.dto.EventJoinMessage;
+import com.java.backend.domain.promotion.dto.EventResultMessage;
 
 @Component
 public class KafkaProducerUtil {
-	private final KafkaTemplate<String, EventJoinMessage> kafkaTemplate;
-	public KafkaProducerUtil(KafkaTemplate<String, EventJoinMessage> kafkaTemplate) {
-		this.kafkaTemplate = kafkaTemplate;
+	private final KafkaTemplate<String, EventJoinMessage> eventJoinKafkaTemplate;
+	private final KafkaTemplate<String, EventResultMessage> eventResultKafkaTemplate;
+	public KafkaProducerUtil(KafkaTemplate<String, EventJoinMessage> kafkaTemplate,
+		KafkaTemplate<String, EventJoinMessage> eventJoinKafkaTemplate,
+		KafkaTemplate<String, EventResultMessage> eventResultKafkaTemplate) {
+		this.eventJoinKafkaTemplate = eventJoinKafkaTemplate;
+		this.eventResultKafkaTemplate = eventResultKafkaTemplate;
 	}
 
-	public void send(String topic, String key, EventJoinMessage value) {
-		CompletableFuture<SendResult<String,EventJoinMessage>> future = kafkaTemplate.send(topic, key, value );
+	public void sendEventJoinMessage(String topic, String key, EventJoinMessage value) {
+		CompletableFuture<SendResult<String,EventJoinMessage>> future = eventJoinKafkaTemplate.send(topic, key, value );
 
 		future.whenComplete((result, ex) -> {
 			if (ex == null) {
@@ -25,5 +30,17 @@ public class KafkaProducerUtil {
 			}
 		});
 
+	}
+
+	public void sendEventResultMessage(String topic, String key, EventResultMessage value){
+		CompletableFuture<SendResult<String,EventResultMessage>> future = eventResultKafkaTemplate.send(topic,key,value);
+
+		future.whenComplete( (result,ex) -> {
+			if (ex == null) {
+				System.out.println("성공");
+			} else {
+				System.out.println("실패");
+			}
+		});
 	}
 }
