d7f74b67c87c46921158d95738ba85cd43b65a32
woosungking <abuga991008@gmail.com>
2025-06-07 20:10:59 +0900

    refactor:

--- src/main/java/com/java/backend/domain/promotion/message/PromotionEventConsumerKafka.java
+++ src/main/java/com/java/backend/domain/promotion/message/PromotionEventConsumerKafka.java
@@ -6,12 +6,14 @@ import org.springframework.stereotype.Component;
 import com.java.backend.domain.promotion.dto.EventJoinMessage;
 import com.java.backend.domain.promotion.dto.EventJoinRequestDto;
 import com.java.backend.domain.promotion.service.EventService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
-public class PromotionEventConsumerKafka implements PromotionEventConsumer{
+public class PromotionEventConsumerKafka implements PromotionEventConsumer {
 	private final EventService eventService;
 	private static final String TOPIC = "forever_mysql_db.forever_mysql_db.outbox_event";
 	private static final String SUMMER_EVENT = "SummerEvent";
+	private final ObjectMapper objectMapper = new ObjectMapper();
 
 	public PromotionEventConsumerKafka(EventService eventService) {
 		this.eventService = eventService;
@@ -20,13 +22,18 @@ public class PromotionEventConsumerKafka implements PromotionEventConsumer{
 	@Override
 	@KafkaListener(topics = TOPIC,
 		containerFactory = "kafkaListenerContainerFactory")
-	public void promotionEventJoinConsumer(EventJoinMessage message) {
-		switch (message.getPromotionKey()){
-			case SUMMER_EVENT:
-				eventService.joinEvent(message.getEventJoinRequestDto());
-				break;
-			default:
-				break;
+	public void promotionEventJoinConsumer(String message) {
+		try {
+			EventJoinMessage eventJoinMessage = objectMapper.readValue(message, EventJoinMessage.class);
+			switch (eventJoinMessage.getPromotionKey()) {
+				case SUMMER_EVENT:
+					eventService.joinEvent(eventJoinMessage.getEventJoinRequestDto());
+					break;
+				default:
+					break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
