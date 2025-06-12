7e7b02f045e4fd1f523ada0907e8091b1fb2f5a5
woosungking <abuga991008@gmail.com>
2025-06-12 21:54:46 +0900

    feat: updateUserPoint

--- src/main/java/com/java/backend/domain/member/user/repository/OliveUserRepository.java
+++ src/main/java/com/java/backend/domain/member/user/repository/OliveUserRepository.java
@@ -1,14 +1,23 @@
-package com.java.backend.domain.user.repository;
+package com.java.backend.domain.member.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.java.backend.domain.user.entity.User;
+import com.java.backend.domain.member.user.entity.User;
+
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface OliveUserRepository extends JpaRepository<User, String> {
     @Query("SELECT u.pass_word FROM User u WHERE u.userId = :userId")
     String findPassWordByUserId(@Param("userId") String userId);
     Optional<User> findByUserId(String userId);
+    Optional<User> findByUserId2(Long userId);
+
+    @Modifying
+    @Query("UPDATE User u SET u.point = :point WHERE u.id = :userId")
+    void updateUserPoint(@Param("userId") Long userId, @Param("point") Integer point);
+
 }
