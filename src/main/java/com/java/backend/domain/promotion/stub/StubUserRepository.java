23716bfd830d49db0dadf1904e1c11c00d6bd012
woosungking <abuga991008@gmail.com>
2025-06-12 21:55:42 +0900

    refactor:

--- src/main/java/com/java/backend/domain/promotion/stub/StubUserRepository.java
+++ src/main/java/com/java/backend/domain/promotion/stub/StubUserRepository.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.java.backend.domain.user.entity.User;
+import com.java.backend.domain.member.user.entity.User;
 
 @Repository
 public interface StubUserRepository extends JpaRepository<User,Long> {
