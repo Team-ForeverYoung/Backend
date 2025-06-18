f7efe49b2f42a5a9366675dae33757160c314342
woosungking <abuga991008@gmail.com>
2025-06-19 00:15:15 +0900

    feat:

--- src/main/java/com/java/backend/global/config/RedisConfig.java
+++ src/main/java/com/java/backend/global/config/RedisConfig.java
@@ -1,9 +1,12 @@
 package com.java.backend.global.config;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class RedisConfig {
@@ -11,5 +14,16 @@ public class RedisConfig {
 	public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
 		return new StringRedisTemplate(connectionFactory);
 	}
+
+	// @Bean
+	// public Executor redisWorkerExecutor() {
+	// 	ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+	// 	executor.setCorePoolSize(8);
+	// 	executor.setMaxPoolSize(16);
+	// 	executor.setQueueCapacity(1000);
+	// 	executor.setThreadNamePrefix("redis-worker-");
+	// 	executor.initialize();
+	// 	return executor;
+	// }
 }
 
